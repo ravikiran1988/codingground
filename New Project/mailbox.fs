4504e675a0be06b38821d4c7d531374c7b8cde40
Ravi Kiran <ravikiran1988@users.noreply.github.com>
2014-11-25 19:30:35 -0600

    Update mailbox.fs

--- New Project/mailbox.fs
+++ New Project/mailbox.fs
@@ -1,8 +1,17 @@
-[object Object]
-
 open System.Threading
-let doubler = MailBoxProcessor.Start(fun(inbox) ->
+
+let doubler = MailboxProcessor.Start(fun(inbox: obj MailboxProcessor) ->
     async{
-        
+        let rec act() =
+            let message = Async.RunSynchronously(inbox.Receive())
+            match message with
+            | :? string as msg -> printfn "You gave a %s" msg
+            | :? int as value -> printfn "Double of value is %d" (value*2)
+            act()
+        act()
     }
 )
+printfn  "In Main %d" Thread.CurrentThread.ManagedThreadId
+[1;2;3;4;5] |> List.iter(fun e -> doubler.Post(e))
+doubler.Post("Hello")
+Thread.Sleep(2000)
